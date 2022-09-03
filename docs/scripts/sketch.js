d120b34da6d0251194e33bab240f259dea213cce
UNSA-MCC-2022 <rparedesro@unsa.edu.pe>
2022-09-03 11:39:17 -0500

    Update sketch.js

--- docs/scripts/sketch.js
+++ docs/scripts/sketch.js
@@ -1,12 +1,14 @@
+const width = 500; //250;
+const height = 500; //200;
+
 function setup() {
-    let width = 500; //250;
-    let height = 500; //200;
+
     let kdTreeCanvas = createCanvas(width, height);
     kdTreeCanvas.parent("KdTreeCanvas");
 
     background(0);
     for (var x = 0; x < width; x += width / 10) {
-        for (var y = 0; y < height; y += height / 5) {
+        for (var y = 0; y < height; y += height / 10) {
             stroke(125, 125, 125);
             strokeWeight(1);
             line(x, 0, x, height);
@@ -16,13 +18,13 @@ function setup() {
 
     var data = [];
     for (let i = 0; i < 12; i++) {
-        var x = Math.floor(Math.random() * height);
+        var x = Math.floor(Math.random() * width);
         var y = Math.floor(Math.random() * height);
         data.push([x, y]);
 
         fill(255, 255, 255);
         circle(x, height - y, 7); // 200 -y para q se dibuje apropiadamente
-        textSize(8);
+        textSize(12);
         text(x + ',' + y, x + 5, height - y);// 200 -y para q se dibuje apropiadamente
     }
 
